f1f8e70e2fa28c0eaf5ce98e4c604602d90249e3
Jonhathan Rolando Rodas <jrodas4044@gmail.com>
2024-09-10 03:41:40 -0600

    Google Analitycs id

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
           </div>
         </footer>
       </body>
-      <GoogleAnalytics gaId="G-JS4CV1B79S" />
+      <GoogleAnalytics gaId="" />
     </html>
   );
 }
